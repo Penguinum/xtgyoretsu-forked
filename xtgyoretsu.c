09d4b127719b6f2037fc184960a60b4f44e0fce1
slovazap <nobody@nowhere.com>
2014-08-21 20:14:45 +0400

    Properly add license

--- xtgyoretsu.c
+++ xtgyoretsu.c
@@ -1,8 +1,21 @@
 /*
- * XT gyoretsu v0.2
- * 2013 - 2014 (c) Kurashov A.K., Kostyukov D.A. and Yakushev D.O.
- * licensed under GNU GPLv3
+ * XT gyoretsu
+ * Copyright (C) 2013-2014  Kurashov A.K., Kostyukov D.A. and Yakushev D.O.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include <curses.h>
 #include <stdlib.h>
 #include <string.h>
